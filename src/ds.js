0da34d18613ce42d556cf5d28934e542a554cf8f
Yeonjj <petechoi10@gmail.com>
2018-12-01 14:57:32 +0900

    Edit: latest commit

--- src/ds.js
+++ src/ds.js
@@ -1,17 +1,13 @@
 'use strict'
 const __dev__ = true
 
-const DPComponent = (()=>{
+const DSOperation = (()=>{
 
-    const _edits = new WeakMap()
     const _diff = new WeakMap()
     const _patch = new WeakMap()
 
-
-    class DPComponent {
+    class DSOperation {
         constructor(diff, patch){
-            _edits.set(this, [])
-
             if(!_diff)
                 throw new SyntaxError(`diff is not implemented! you should pass it as constructor param`)
             _diff.set(this, diff)
@@ -20,41 +16,44 @@ const DPComponent = (()=>{
                 throw new SyntaxError(`patch is not implemented! you should pass it as constructor param`)
             _patch.set(this, patch)
         }
-
-        get edits() {
-            return _edits.get(this)
-        }
-
         diff(oldContent, newContent) {
-            _diff.get(this).call(this, oldContent, newContent)
+            return _diff.get(this)(oldContent, newContent)
         }
-
-        patch(oldContent) {
-            return _patch.get(this).call(this, oldContent)
+        patch(oldContent, edits) {
+            return _patch.get(this)(oldContent, edits)
         }
     }
-
-    return DPComponent
+    return DSOperation
 })()
 
-// ajax connection
-const DSPipelineComponent = (()=>{
+// pipeline
+const DSPipeline = (()=>{
     // assume that content is always HETML element
+    const _edits = new WeakMap()
+    const _myVer = new WeakMap()
+    const _yourVer = new WeakMap()
     const _content = new WeakMap()
+    const _DSOperation = new WeakMap()
     const _xhr = new WeakMap()
-    class DSPipelineComponent {
-        constructor(content, url){
-            _content.set(this, content)
+    class DSPipeline {
+        constructor(contentType, url){
             _url.set(this, url)
             _xhr.set(this, new XMLHttpRequest())
+            this.createOperation(contentType)
         }
-        start(){
-            setInterval(this.sendEditsWithVersionNumber(url),1000)
-            return this
+        //start pipline
+        run(){
+            setInterval(,1000)
         }
         sendEditsWithVersionNumber(url, edits){
             // axjs send date [edits, my_ver, your_ver]
             throw new SyntaxError(`sendEditswithversionnumber is not implemented!`)
+        }
+        createOperation(type){
+            if(type == "text"){
+                const dsText = new DSTextOperation()
+                _DSOperation.set(this, new DSOperation(dsText.diff, dsText.patch))
+            }
         }
     }
     return DSPipelineComponent
@@ -62,19 +61,19 @@ const DSPipelineComponent = (()=>{
 
 // client ds
 const DSPipelineClient = (()=>{
-    class DSPipelineClient extends DSPipelineComponent{
+    class DSPipelineClient extends DSPipeline{
         constructor(){
         }
         sendEditsWithVersionNumber(url, edits){
-
         }
     }
     return DSPipelineClient
 })()
 
-
+// server ds
 const DSPipelineServer = (()=>{
-    class DSPipelineClient extends DSPipelineComponent{
+    const _
+    class DSPipelineServer {
         constructor(){
         }
         sendEditsWithVersionNumber(url, edits){
@@ -93,17 +92,17 @@ const DSObject = (()=>{
 })()
 
 if(__dev__){
-    exports.DPComponent = DPComponent
+    exports.DSOperation = DSOperation
     exports.DSPipelineComponent = DSPipelineComponent
 }
 exports.DSObject = DSObject
 
 
 // This is an user side code.
-// TODO: should make this be coded on user side as an inheritance class of DPComponent
+// TODO: should make this be coded on user side as an inheritance class of DSOperation
 const diff_match_patch = require('./diff-match-patch.js')
 
-const implementDS = function() {
+const DSTextOperation = function() {
     const _dmp = new diff_match_patch()
     return {
         diff : function (oldText, newText){
@@ -112,23 +111,18 @@ const implementDS = function() {
             if (diff.length > 2) {
                 _dmp.diff_cleanupSemantic(diff);
             }
-
             const edit_list = _dmp.patch_make(oldText, newText, diff)
-            this.edits.push(edit_list)
+            return edit_list
         },
-        patch : function (oldText){
+        patch : function (oldText, edits){
             let result = ''
-            for(const edit of this.edits){
+            for(const edit of edits){
                 if (result)
                     result = _dmp.patch_apply(edit, result[0])
                 else
                     result = _dmp.patch_apply(edit, oldText)
-            }
+
             return result[0]
         }
     }
 }
-
-const implemented = new implementDS()
-exports.textComponent = new DPComponent(implemented.diff, implemented.patch)
-
